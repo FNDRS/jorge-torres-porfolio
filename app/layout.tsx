73d9ebd4cf2200bd6710f2eae55b942b8f4fafc8
carlos.pineda <pinedalberto@hotmail.com>
2025-07-08 20:53:23 -0600

    feat: increase border width in RootLayout for enhanced visibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import NavMenu from "@/components/nav-menu"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="w-full border-black border-8 bg-black text-white relative">
+      <body className="w-full border-black border-[20px] bg-black text-white relative">
         <Header />
         {children}
         <NavMenu />
