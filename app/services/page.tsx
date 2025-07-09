6c85121ff5049dc902aba28375f5ab356f0f082f
carlos.pineda <pinedalberto@hotmail.com>
2025-07-09 16:32:25 -0600

    feat: update typography to use Red Hat Display font in page.tsx for consistency

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -28,17 +28,17 @@ const ServicesPage = () => {
         </div>
       </section>
       <section>
-        <h2 className="font-inter font-bold text-[30px] leading-[100%] tracking-[0] text-left my-4">MY OWN VISION</h2>
+        <h2 className="font-bold text-[30px] leading-[100%] tracking-[0] text-left my-4 font-redhat">MY OWN VISION</h2>
         <div className="col-span-3 text-red-600 relative bg-[url('/assets/IMG_18.png')] p-2 bg-cover bg-center h-[996px] flex justify-center items-center">
-          <h2 className="z-10 font-bold text-[80px]">
+          <h2 className="z-10 font-bold text-[80px] text-center font-redhat">
             THE WORLD THROUGH
             <br />
             MY EYES
           </h2>
-          <p className="absolute top-6 left-6 text-inter text-[50px] z-10">Explore</p>
-          <p className="absolute top-6 left-1/2 -translate-x-1/2 text-inter text-[50px] z-10">Discover</p>
-          <p className="absolute top-6 right-6 text-inter text-[50px] z-10">Create</p>
-          <p className="absolute bottom-6 left-6 text-inter text-[35px] z-10">
+          <p className="absolute top-6 left-6 text-[50px] z-10 font-redhat">Explore</p>
+          <p className="absolute top-6 left-1/2 -translate-x-1/2 text-[50px] font-redhat z-10">Discover</p>
+          <p className="absolute top-6 right-6 text-[50px] z-10 font-redhat">Create</p>
+          <p className="absolute bottom-6 left-6 text-[35px] z-10 font-redhat">
             <i>
               Creating visuals that connect with
               <br />
@@ -47,10 +47,10 @@ const ServicesPage = () => {
               emotion.
             </i>
           </p>
-          <p className="absolute bottom-6 right-6 text-inter text-[35px] z-10">
+          <p className="absolute bottom-6 right-6 text-[35px] z-10 font-redhat">
             <i>1% better every day</i>
           </p>
-          <p className="absolute bottom-[350px] right-6 text-inter text-[35px] z-10 text-right">
+          <p className="absolute bottom-[350px] right-6 text-[35px] z-10 text-right font-redhat">
             <i>
               Weaving ideas with souls who long
               <br /> to transform the world.
@@ -60,9 +60,9 @@ const ServicesPage = () => {
         <div className="h-[345px] flex">
           <div className="bg-[url('/assets/IMG_19.png')] w-1/2"></div>
           <div className="bg-[url('/assets/IMG_20.png')] w-1/2"></div>
-          <p className="absolute bottom-6 left-6 text-inter text-[50px] z-10 text-red-600">Passion</p>
-          <p className="absolute bottom-6 left-[50%] translate-x-[-50%] text-inter text-[50px] z-10 text-red-600">Discipline</p>
-          <p className="absolute bottom-6 right-6 text-inter text-[50px] z-10 text-red-600">Purpose</p>
+          <p className="absolute bottom-6 left-6 text-[50px] z-10 text-red-600 font-redhat">Passion</p>
+          <p className="absolute bottom-6 left-[50%] translate-x-[-50%] text-[50px] z-10 text-red-600 font-redhat">Discipline</p>
+          <p className="absolute bottom-6 right-6 text-[50px] z-10 text-red-600 font-redhat">Purpose</p>
         </div>
       </section>
     </>
